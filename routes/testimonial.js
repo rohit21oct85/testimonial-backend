1fb2af6ad55d88ed42a00f9cf45850212313e42a
Rohit Singh <rohit21oct85@gmail.com>
2021-08-01 08:03:08 +0530

    code added Procfile

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -7,5 +7,4 @@ router.post('/create/testimonial', controller.createTestimonial);
 router.get('/view-all/testimonials/:status?', controller.getTestimonials);
 router.patch('/update/testimonial/:testimonial_id?', controller.updateTestimonial);
 router.delete('/delete/testimonial/:testimonial_id?', controller.deleteTestimonial);
-
 module.exports = router;
