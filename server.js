c6740348b6a0084fb0e99670a184b30b6d6ba5d8
Rohit Singh <rohit21oct85@gmail.com>
2021-08-01 08:24:20 +0530

    config file changed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('./app.js');
 const http = require('http');
-const PORT = process.env.SERVER_PORT || 8080;
 const server = http.createServer(app);
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
 
-server.listen(PORT)
+
+server.listen(port,host)
 server.on('listening', () => {
-      console.info(`server running on port ${PORT}`);
+      console.info(`server running on  ${host}:${port}`);
 })
